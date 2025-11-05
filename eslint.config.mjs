98cf9adde7ee10e49bb38624cdc419cedbf213a3
Naufal <naufalbhurr@gmail.com>
2025-11-05 18:55:53 +0700

    Integrate TypeScript support in ESLint configuration by adding 'eslint-config-next/typescript' and updating rules for improved code quality and type safety.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextTs from 'eslint-config-next/typescript'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,17 +10,26 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      "@typescript-eslint/no-explicit-any": "off"
+    },
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [
+    ignores: ([
       "node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
       "next-env.d.ts",
-    ],
+    ]),
   },
-];
+]);
 
 export default eslintConfig;
